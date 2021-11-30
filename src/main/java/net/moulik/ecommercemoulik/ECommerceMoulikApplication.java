e4de6e0b36f32a0422e7204ce7ba118ae073032a
aminul <aminul@dohatec.com.bd>
2021-11-30 11:54:14 +0600

    project initialized and configured

---
+++ src/main/java/net/moulik/ecommercemoulik/ECommerceMoulikApplication.java
@@ -0,0 +1,13 @@
+package net.moulik.ecommercemoulik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMoulikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMoulikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/moulik/ecommercemoulik/ECommerceMoulikApplicationTests.java
@@ -0,0 +1,13 @@
+package net.moulik.ecommercemoulik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMoulikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
