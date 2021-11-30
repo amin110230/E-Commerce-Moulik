5447a9b6d37418a038199ffa6958a3fe038e2d50
aminul <aminul@dohatec.com.bd>
2021-11-30 13:56:58 +0600

    initial

---
+++ src/main/java/net/moulik/ecommercemoulik/ECommerceMoulikApplication.java
@@ -0,0 +1,14 @@
+package net.moulik.ecommercemoulik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMoulikApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Project Started");
+		SpringApplication.run(ECommerceMoulikApplication.class, args);
+	}
+
+}
